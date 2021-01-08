5f1ed8ddb0c80dc0345f3b417bb6eba0c2b33f50
Collins Koech <collinskoechck34@gmail.com>
2021-01-08 10:12:13 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { useState } from 'react';
+import { Dialog } from '@reach/dialog';
+import '@reach/dialog/styles.css'
 
-export default function Home() {
+
+export default function Home(props) {
+  const [showDialog, setShowDialog] = useState(false)
+  const open = () => setShowDialog(true)
+  const close = () => setShowDialog(false)
   return (
     <div className={styles.container}>
       <Head>
@@ -9,7 +16,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      
+      <main>
+      <div>
+      <button onClick={open}>Open Dialog</button>
+      <Dialog isOpen={showDialog} onDismiss={close}>
+        <button className="close-button" onClick={close}>
+          <VisuallyHidden>Close</VisuallyHidden>
+          <span aria-hidden>×</span>
+        </button>
+        <p>Hello there. I am a dialog</p>
+      </Dialog>
+    </div>
+      </main>
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
