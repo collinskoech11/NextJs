867dd1f88421ab5632cfddb4ce596a5855cccdd6
Collins Koech <collinskoechck34@gmail.com>
2021-01-09 10:48:23 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,8 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { useState } from 'react';
-import '@reach/dialog/styles.css'
 
 
-export default function Home(props) {
-  const [showDialog, setShowDialog] = useState(false)
-  const open = () => setShowDialog(true)
-  const close = () => setShowDialog(false)
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
@@ -16,16 +11,7 @@ export default function Home(props) {
       </Head>
 
       <main>
-      <div>
-      <button onClick={open}>Open Dialog</button>
-      <Dialog isOpen={showDialog} onDismiss={close}>
-        <button className="close-button" onClick={close}>
-          <VisuallyHidden>Close</VisuallyHidden>
-          <span aria-hidden>×</span>
-        </button>
-        <p>Hello there. I am a dialog</p>
-      </Dialog>
-    </div>
+      
       </main>
       <footer className={styles.footer}>
         <a
